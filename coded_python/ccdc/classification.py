d4c2494b52f0dd2e4429d61d2bc66ab1e25b0d9b
John Dilger <johnjdilger@gmail.com>
2021-10-14 18:10:40 -0500

    added accuracyProcedure

--- coded_python/ccdc/classification.py
+++ coded_python/ccdc/classification.py
@@ -83,45 +83,41 @@ def subsetTraining(trainingData, trainProp, seed, classProperty):
 
 
 
-# # /**
-# # * Calculate accuracy metrics using a subset of the training data
-# # * @param {ee.FeatureCollection} trainingData training data
-# # * @param {ee.Image} imageToClassify ccdc coefficient stack to classify
-# # * @param {array} predictors list of predictor ables as strings
-# # * @param {array} bandNames list of band names to classify
-# # * @param {array} ancillary list of ancillary predictor data
-# # * @param {ee.Classifier} classifier earth engine classifier with parameters
-# # * @param {string} [classProperty='LC_Num'] attribute name with land cover label
-# # * @param {number} [seed='random'] seed to use for the random column generator
-# # * @param {float} [trainProp=.4] proportion of data to use subset for training
-# # * @returns {ee.ConfusionMatrix} a confusion matrix as calculated by the train/test subset
-# # */
-# def accuracyProcedure(trainingData, imageToClassify, predictors, bandNames, 
-#     ancillary, classifier, classProperty, seed=None, trainProp):
-#     if seed is None:
-#         seed = random.randint(0,1000)
-#     trainProp =  .4 
-#     classProperty = 'LC_Num'
-#     trainingData = trainingData.randomColumn('random',seed).sort('random')
-#     trainingData = subsetTraining(trainingData, trainProp, seed, classProperty) 
-#     testSubsetTest = trainingData.filterMetadata('train','equals',0)
+# /**
+# * Calculate accuracy metrics using a subset of the training data
+# * @param {ee.FeatureCollection} trainingData training data
+# * @param {ee.Image} imageToClassify ccdc coefficient stack to classify
+# * @param {array} predictors list of predictor ables as strings
+# * @param {array} bandNames list of band names to classify
+# * @param {array} ancillary list of ancillary predictor data
+# * @param {ee.Classifier} classifier earth engine classifier with parameters
+# * @param {string} [classProperty='LC_Num'] attribute name with land cover label
+# * @param {number} [seed='random'] seed to use for the random column generator
+# * @param {float} [trainProp=.4] proportion of data to use subset for training
+# * @returns {ee.ConfusionMatrix} a confusion matrix as calculated by the train/test subset
+# */
+def accuracyProcedure(trainingData, imageToClassify, predictors, bandNames, 
+    ancillary, classifier, classProperty, seed, trainProp):
+    if seed is None:
+        seed = random.randint(0,1000)
+    trainProp =  .4 
+    classProperty = 'LC_Num'
+    trainingData = trainingData.randomColumn('random',seed).sort('random')
+    trainingData = subsetTraining(trainingData, trainProp, seed, classProperty) 
+    testSubsetTest = trainingData.filterMetadata('train','equals',0)
 
-#   testSubsetTrain = trainingData.filterMetadata('train','equals',1)
+    testSubsetTrain = trainingData.filterMetadata('train','equals',1)
 
-#   inputList = getInputFeatures(1, imageToClassify, predictors, bandNames, ancillary)
-#   inputFeatures = ee.List(inputList).get(0)
+    inputList = getInputFeatures(1, imageToClassify, predictors, bandNames, ancillary)
+    inputFeatures = ee.List(inputList).get(0)
 
-#     // inputFeatures = inputList[0]
+    # // Train the classifier
+    trained = classifier.train(**{
+    'features': testSubsetTrain,
+    'classProperty': classProperty,
+    'inputProperties': inputFeatures
+    })
 
-#   // Train the classifier
-#   trained = classifier.train({
-#     features: testSubsetTrain,
-#     classProperty: classProperty,
-#     inputProperties: inputFeatures
-#   })
-#   classified = testSubsetTest.classify(trained)
-#   confMatrix = classified.errorMatrix(classProperty, 'classification')
-#   // return [confMatrix, trained]
-#   return confMatrix
-
-# }
+    classified = testSubsetTest.classify(trained)
+    confMatrix = classified.errorMatrix(classProperty, 'classification')
+    return confMatrix
