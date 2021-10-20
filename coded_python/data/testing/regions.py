885c0c1fd587066c5caa6c01ec7114e2f99c5258
John Dilger <johnjdilger@gmail.com>
2021-10-20 12:06:56 -0500

    remove not needed import

--- coded_python/data/testing/regions.py
+++ coded_python/data/testing/regions.py
@@ -1,6 +1,5 @@
 # regions.py
 import ee
-from ee.featurecollection import FeatureCollection
 ee.Initialize()
 class test_study_area:
     region : ee.FeatureCollection = ee.FeatureCollection('projects/python-coded/assets/tests/regions/test_geometry')
